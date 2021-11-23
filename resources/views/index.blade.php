5365772111c8c573d3a95a4daf21f46b98ca6d88
shimpei ohtani <ohtani.shimpei@gmail.com>
2021-11-23 19:23:53 +0900

    DB

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
 
     <h1>BBS</h1>
     <ul>
-        @forelse ($posts as $index => $post)
+        @forelse ($posts as $post)
         <li>
-            <a href="{{route('posts.show',$index)}}">{{$post}}</a>
+            <a href="{{route('posts.show',$post)}}">{{$post->title}}</a>
         </li>
         @empty
         <li>no post</li>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class,'index'])
     ->name('posts.index');
-Route::get('/posts/{id}', [PostController::class,'show'])
+
+Route::get('/posts/{post}', [PostController::class,'show'])
     ->name('posts.show');

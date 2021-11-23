fed0962927f6fab52a91c6895dd80045229241de
shimpei ohtani <ohtani.shimpei@gmail.com>
2021-11-23 23:34:29 +0900

    CRUD

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,28 @@
     <div class="back-link">
         &laquo;<a href="{{route('posts.index')}}">Back</a>
     </div>
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
+    <h1>
+        <span>{{ $post->title }}</span>
+        <a href="{{ route('posts.edit',$post) }}">[Edit]</a>
+        <form action="{{ route('posts.destroy',$post) }}" method="post" id="delete_post">
+            @method('DELETE')
+            @csrf
+            <button class="btn">[x]</button>
+        </form>
+    </h1>
+    <p>{!! nl2br(e($post->body)) !!}</p>
+    <script>
+        'use strict';
+        {
+            document.getElementById('delete_post').addEventListener('submit',e => {
+                e.preventDefault();
+
+                if(!confirm('Sure to delete?')) {
+                    return;
+                }
+
+                e.target.submit();
+            });
+        }
+    </script>
 </x-layout>
